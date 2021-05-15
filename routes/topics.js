79def51e6e80f225e4b8109212b7d3e2e10c74bb
Kelsey Mwongeli <kelseykm.git@gmail.com>
2021-05-15 20:55:03 +0300

    Update topics.js

--- routes/topics.js
+++ routes/topics.js
@@ -112,13 +112,11 @@ router.post('/topic/:topicName', ensureFields, (req, res) => {
 	let voiceNoteEntry = req.file ? req.file["filename"] : null;
 
 	if (req.file) {
-		(async () => {
-			await aesCfbCipher.encryptFile(
-				path.join(tempDir, req.file["filename"]),
-				path.join(process.env.PWD, `/db/voice_notes/${req.file["filename"]}`)
-			);
-			fs.rm(path.join(tempDir, req.file["filename"]));
-		})()
+		aesCfbCipher.encryptFile(
+			path.join(tempDir, req.file["filename"]),
+			path.join(process.env.PWD, `/db/voice_notes/${req.file["filename"]}`)
+		);
+		setTimeout(() => fs.rm(path.join(tempDir, req.file["filename"])), 500);
 	}
 
 	db.get('SELECT entry_name FROM notes WHERE user_id = ? AND topic_name = ? AND entry_name = ?',
